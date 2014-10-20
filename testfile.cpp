09623ec917d01bcd490f46d2452febd0300cc1c5
angeliquenonan <angeliquenonan@yahoo.com>
2014-10-20 19:51:12 +0800

    Create testfile.cpp

---
+++ testfile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	cout << "with newline after (1)" << endl;
+	cout << "Hello kjjhlgkgkgWorld! AwesomeHello";
+	cout << "without newline after (1)";
+	cout << "with newline after (2)" << endl;
+	cout << "Hello World!";
+	cout << "without newline after (2)";
+	return 0;
+}

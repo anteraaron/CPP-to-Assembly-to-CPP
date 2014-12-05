8aafa4210ccb41675f87f20bdb0a6319ba757dbb
carlzacher <csviernes@gmail.com>
2014-12-05 14:46:16 +0800

    Create CPPtoASM.java
    Nilagay yung mga gagamiting arraylist at yung method name.

---
+++ CPPtoASM.java
@@ -0,0 +1,16 @@
+public class CPPtoASM{
+  
+  //dito ilalagay lahat ng declared variables
+  private ArrayList<String> variables = new ArrayList<String>();
+  //dito naman yung ilalagay sa ".code" part ng ASM
+	private ArrayList<String> code = new ArrayList<String>();
+	//dito yung ilalagay sa ".data" na part
+	private ArrayList<String> data = new ArrayList<String>();
+	private String blank = "";
+
+  //nagiisang method para madali
+  public void convertCPPtoASM() throws FileNotFoundException{
+  
+  
+  }
+}
